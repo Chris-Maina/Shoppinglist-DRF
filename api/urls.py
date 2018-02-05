411f807fc693e39da3aa35dfd40b129f1dc65afe
Chris-Maina <krismaish34@gmail.com>
2018-02-05 20:35:29 +0300

    [Feature] Add url for register.

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from api.views import ShoppinglistView, ShoppinglistDetailView
+from api.views import ShoppinglistView, ShoppinglistDetailView, UserRegisterView
 
 urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name="register"),
     path('shoppinglists/', ShoppinglistView.as_view(), name="create_list"),
     path('shoppinglists/<int:pk>/', ShoppinglistDetailView.as_view(), name="shoppinglist_details")
 ]
