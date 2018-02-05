3beca528b96d424ba3cb7ea917359c947bb68fdc
Chris-Maina <krismaish34@gmail.com>
2018-02-05 21:00:13 +0300

    [Feature] Add UserRegisterView to handle post and get methods.

--- api/views.py
+++ api/views.py
@@ -1,11 +1,33 @@
 """ api/views.py """
 
-from .models import Shoppinglist
-from .serializers import ShoppinglistSerializer
-from rest_framework import generics, response
-
+from .models import Shoppinglist, User
+from .serializers import ShoppinglistSerializer, UserSerializer
+from rest_framework import generics, response, status, views
+from rest_framework.decorators import api_view
 
 # Create your views here.
+class UserRegisterView(views.APIView):   
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer()
+        return response.Response(serializer.data)
+
+    def post(self, request):
+        serialized = UserSerializer(data=request.data)
+        if serialized.is_valid(raise_exception=True):
+            serialized.save()
+            return response.Response(serialized.data, status=status.HTTP_201_CREATED)
+        else:
+            return response.Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        serialized = UserSerializer(data=request.data)
+        if serialized.is_valid(raise_exception=True):
+            serialized.save()
+            return response.Response(serialized.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return response.Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
 class ShoppinglistView(generics.ListCreateAPIView):
     """ Defines post and get behaviour of Shoppinglist model """
     queryset = Shoppinglist.objects.all()
@@ -13,7 +35,7 @@ class ShoppinglistView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
-        serializer.save()
+        serializer.save(owner=self.request.user)
 
 class ShoppinglistDetailView(generics.RetrieveUpdateDestroyAPIView):
     """ Defines get a single list,put and delete behaviour of Shoppinglist model """
